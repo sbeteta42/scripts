ff2afca515b2ca4b3664b666e88d756b88cb08f8
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-01-09 09:32:11 +0100

    Add files via upload

---
+++ install_soft_client_windows.bat
@@ -0,0 +1,3 @@
+@cls
+@echo - installation des logiciels essentiels pour les clients WINDOWS... Please wait !
+choco install googlechrome firefox putty winscp 7zip.install notepadplusplus.install vlc xnview thunderbird dotnet4.5 wireshark -y
---
+++ install_soft_server_windows.bat
@@ -0,0 +1,3 @@
+@cls
+@echo - installation des logiciels essentiels pour les Serveurs WINDOWS... Please wait !
+choco install googlechrome firefox putty winscp 7zip.install notepadplusplus.install thunderbird wireshark -y
