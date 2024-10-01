f2b2a6f98d62d21d787e8d7344ece7667b2592ea
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-10-01 07:53:47 +0200

    Update joindreAuDomaine.ps1

--- joindreAuDomaine.ps1
+++ joindreAuDomaine.ps1
@@ -1,11 +1,6 @@
 ﻿# sbeteta@beteta.org
 #Set-ExecutionPolicy RemoteSigned -Force
-
-
 $user = "administrateur@formation.lan"
 $password = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ($User, $password)
-
 Add-Computer -NewName pc01 -DomainName formation.lan -Credential $Cred -Restart -Force
-
- 
