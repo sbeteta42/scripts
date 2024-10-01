658a9a0563a29cca6069aa20e65fb9983c0df056
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-10-01 07:53:02 +0200

    Update joindreAuDomaine.ps1

--- joindreAuDomaine.ps1
+++ joindreAuDomaine.ps1
@@ -2,10 +2,10 @@
 #Set-ExecutionPolicy RemoteSigned -Force
 
 
-$user = "administrateur@Formation.lan"
+$user = "administrateur@formation.lan"
 $password = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential ($User, $password)
 
-Add-Computer -NewName pc01 -DomainName Formation.lan -Credential $Cred -Restart -Force
+Add-Computer -NewName pc01 -DomainName formation.lan -Credential $Cred -Restart -Force
 
  
