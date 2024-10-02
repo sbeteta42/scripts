b53d5b8ab5a388b3bab30d1e21d5e7540582984c
sbeteta42 <137415087+sbeteta42@users.noreply.github.com>
2024-10-02 21:21:52 +0200

    Add files via upload

--- install_vmware_workstation.sh
+++ install_vmware_workstation.sh
@@ -1,5 +1,5 @@
 
 echo "Installation de Vmware VMware-Workstation-Full-17"
 sleep 5
-VMware-Workstation-Full-17.5.0-22583795.x86_64.bundle --console --set-setting vmware-workstation serialNumber HF200-0W05K-089X8-4R1EK-032J0 --set-setting vmware-installer installShortcuts yes
+VMware-Workstation-Full-17.5.0-22583795.x86_64.bundle --console --deferred-gtk --set-setting vmware-workstation serialNumber HF200-0W05K-089X8-4R1EK-032J0 --set-setting vmware-installer installShortcuts yes
 echo "Installation terminé"
